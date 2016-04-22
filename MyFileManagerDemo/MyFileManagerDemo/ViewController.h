b32052c6fb8dd12cee660749c42f099ba70389ec
fushunlong <fushunlong@haizitong.com>
2016-04-22 16:13:31 +0800

    verson 0.0.1

---
+++ MyFileManagerDemo/MyFileManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFileManagerDemo
+//
+//  Created by haizitong4 on 16/4/22.
+//  Copyright © 2016年 Ronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFileManagerDemo/MyFileManagerDemoTests/MyFileManagerDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  MyFileManagerDemoTests.m
+//  MyFileManagerDemoTests
+//
+//  Created by haizitong4 on 16/4/22.
+//  Copyright © 2016年 Ronin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyFileManagerDemoTests : XCTestCase
+
+@end
+
+@implementation MyFileManagerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
